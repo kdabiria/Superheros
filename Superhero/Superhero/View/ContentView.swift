bef9d104e15a5dd65931d3250dc87593d6e964ed
Kamyar Dabiri <kdabiria@uci.edu>
2021-08-02 09:15:20 -0700

    using button instead of navigation

--- Superhero/Superhero/View/ContentView.swift
+++ Superhero/Superhero/View/ContentView.swift
@@ -11,66 +11,77 @@ struct ContentView: View {
     @State var username:String = ""
     @State var password:String = ""
     
+    @State var reg = false
+    
     var body: some View {
-        NavigationView {
-            ZStack {
-                
-                VStack(spacing: 5.0) {
-                    Image("logo")
-                        .resizable()
-                        .scaledToFit()
-                    Spacer()
-                    VStack {
-                        TextField("Username", text:$username)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        SecureField("Password", text:$password)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                    }
-                    
-                    Spacer()
-                    NavigationLink(
-                        destination: Home(),
-                        label: {
-                            Text("Login")
-                                .font(.largeTitle)
-                                .foregroundColor(Color.white)
-                        })
-                    
-                    NavigationLink(
-                        destination: Register(),
-                        label: {
-                            Text("Register")
-                                .font(.title2)
-                                .foregroundColor(Color.white)
-                        })
-//                    Button(action: {
-//                        print("Cliked me")
-//                    }) {
-//                        Text("Login")
-//                            .font(.system(size: 25))
-//                            .foregroundColor(Color.white)
-//    //                        .background(RoundedRectangle(cornerRadius: 8).fill(Color.white))
-//                    }
-                    Spacer()
-                        
-                }
-                .padding(80)
-            }
-            .background(
-                Image("background")
+        ZStack {
+            
+            VStack(spacing: 5.0) {
+                Image("logo")
                     .resizable()
-                    .edgesIgnoringSafeArea(.all)
-                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-                    .blur(radius: 5.0)
+                    .scaledToFit()
+                Spacer()
+                VStack {
+                    TextField("Username", text:$username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    SecureField("Password", text:$password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
                 
-            )
+                Spacer()
+                
+                Button("Login", action: loginButton)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+//                    NavigationLink(
+//                        destination: Home(),
+//                        label: {
+//                            Text("Login")
+//                                .font(.largeTitle)
+//                                .foregroundColor(Color.white)
+//                        })
+                Button("Register", action: {
+                    self.reg.toggle()
+                })
+                    .font(.title2)
+                    .foregroundColor(.white)
+                .sheet(isPresented: $reg, content: {
+                    Register()
+                })
+                
+//                    NavigationLink(
+//                        destination: Register(),
+//                        label: {
+//                            Text("Register")
+//                                .font(.title2)
+//                                .foregroundColor(Color.white)
+//                        })
+                Spacer()
+                    
+            }
+            .padding(80)
         }
-        .navigationViewStyle(StackNavigationViewStyle())
-        
+        .background(
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .blur(radius: 5.0)
+            
+        )
     }
 }
 
+func loginButton() {
+    if let window = UIApplication.shared.windows.first {
+        window.rootViewController = UIHostingController(rootView: Home())
+        window.makeKeyAndVisible()
+    }
+}
 
+//func registerButton() {
+//    reg.toggle()
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
