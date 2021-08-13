8ede4780313a9781f8ac8248161de99e831a0a02
Kamyar Dabiri <kdabiria@uci.edu>
2021-08-13 11:54:29 -0700

    change the UI foe hero details

--- Superhero/Superhero/View/HeroDetailsView.swift
+++ Superhero/Superhero/View/HeroDetailsView.swift
@@ -23,25 +23,30 @@ struct HeroDetailsView: View {
 struct heroList: View {
     var eachHero: heroInfo
     var body: some View {
-        ScrollView {
-            VStack {
-                Group {
-                    Image(systemName: "eye.slash")
-                        .data(url: URL(string: eachHero.image.url)!)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 300)
-                        .cornerRadius(12)
-                        .padding()
-                    Text("Superhero Name: \(eachHero.name)")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    
-                    VStack {
+        VStack {
+            Group {
+                //systemName: "eye.slash"
+                Image(systemName: "eye.slash")
+                    .data(url: URL(string: eachHero.image.url)!)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 300)
+                    .cornerRadius(12)
+//                    .padding(.top, 250)
+                Text("Superhero Name: \(eachHero.name)")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+            .padding()
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack( spacing: 50) {
+                    VStack() {
                         Text("PowersStat")
                             .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                        HStack() {
+                            .padding()
+//                        HStack() {
                             if(eachHero.powerstats.intelligence == "null") {
                                 Text("Intelligence: -")
                                     .font(.title3)
@@ -50,7 +55,7 @@ struct heroList: View {
                                 Text("Intelligence: \(eachHero.powerstats.intelligence)")
                                     .font(.title3)
                             }
-                            Spacer()
+//                            Spacer()
                             if(eachHero.powerstats.strength == "null") {
                                 Text("Intelligence: -")
                                     .font(.title3)
@@ -59,9 +64,10 @@ struct heroList: View {
                                 Text("Strength: \(eachHero.powerstats.strength)")
                                     .font(.title3)
                             }
-                        }
-                        .padding()
-                        HStack {
+//                        }
+//                        .padding()
+                        
+//                        HStack {
                             if(eachHero.powerstats.speed == "null") {
                                 Text("Speed: -")
                                     .font(.title3)
@@ -70,7 +76,7 @@ struct heroList: View {
                                 Text("Speed: \(eachHero.powerstats.speed)")
                                     .font(.title3)
                             }
-                            Spacer()
+//                            Spacer()
                             if(eachHero.powerstats.durability == "null") {
                                 Text("Durability: -")
                                     .font(.title3)
@@ -79,9 +85,9 @@ struct heroList: View {
                                 Text("Durability: \(eachHero.powerstats.durability)")
                                     .font(.title3)
                             }
-                        }
-                        .padding()
-                        HStack {
+//                        }
+//                        .padding()
+//                        HStack {
                             if(eachHero.powerstats.power == "null") {
                                 Text("Power: -")
                                     .font(.title3)
@@ -89,7 +95,7 @@ struct heroList: View {
                                 Text("Power: \(eachHero.powerstats.power)")
                                     .font(.title3)
                             }
-                            Spacer()
+//                            Spacer()
                             if(eachHero.powerstats.combat == "null") {
                                 Text("Combat: -")
                                     .font(.title3)
@@ -97,89 +103,198 @@ struct heroList: View {
                                 Text("Combat: \(eachHero.powerstats.combat)")
                                     .font(.title3)
                             }
+//                        }
+//                        .padding()
+                    }
+                    .frame(maxWidth: 450, maxHeight: .infinity)
+                    .padding()
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.white)
+                            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                    )
+                    
+                    VStack() {
+                        Text("Biography:")
+                            .font(.title)
+                            .padding()
+                        Text("Full Name: \(eachHero.biography.fullName)")
+                            .font(.title3)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+
+                        Text("Place of Birth: \(eachHero.biography.placeOfBirth)")
+                            .font(.title3)
+                            .lineLimit(nil)
+                            .multilineTextAlignment(.center)
+                        
+                        Text("Alter Egos: \(eachHero.biography.alterEgos)")
+                            .font(.title3)
+                            .lineLimit(nil)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Text("Gender: \(eachHero.appearance.gender)")
+                            .font(.title3)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        if eachHero.appearance.race != "null" {
+                            Text("Race: \(eachHero.appearance.race)")
+                                .font(.title3)
+                                .lineLimit(nil)
+                                .multilineTextAlignment(.center)
+
                         }
-                        .padding()
-                    }
-                    
-                    Text("Biography:")
-                        .font(.title)
-                    Text("Full Name: \(eachHero.biography.fullName)")
-                        .font(.title3)
-                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        
-                    Text("Place of Birth: \(eachHero.biography.placeOfBirth)")
-                        .font(.title3)
-                        .multilineTextAlignment(.center)
-//                        .fixedSize(horizontal: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/, vertical: false)
-//                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    Text("Alter Egos: \(eachHero.biography.alterEgos)")
-                        .font(.title3)
-                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    Text("Gender: \(eachHero.appearance.gender)")
-                        .font(.title3)
-                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    if eachHero.appearance.race != "null" {
-                        Text("Race: \(eachHero.appearance.race)")
+                        else {
+                            Text("Race: -")
+                                .font(.title3)
+                                .multilineTextAlignment(.center)
+                        }
+
+                        Text("Eye Color: \(eachHero.appearance.eyeColor)")
                             .font(.title3)
-                            .multilineTextAlignment(.center)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                         
-                    }
-                    else {
-                        Text("Race: -")
+                        Text("Hair Color: \(eachHero.appearance.hairColor)")
                             .font(.title3)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    }
+                    .frame(maxWidth: 450, maxHeight: .infinity)
+                    .padding(.bottom)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.white)
+                            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                    )
+                    
+                    VStack {
+                        Text("Occupation:")
+                            .font(.title)
+                            .padding()
+                        Text("\(eachHero.work.occupation)")
+                            .font(.title3)
+                            .lineLimit(nil)
                             .multilineTextAlignment(.center)
                     }
-//                    Text("Height: \(eachHero.appearance.height.first)")
-//                        .font(.title3)
-//                    Text("Weight: \(eachHero.appearance.weight.first)")
-//                        .font(.title3)
-                    Text("Eye Color: \(eachHero.appearance.eyeColor)")
-                        .font(.title3)
-                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .frame(maxWidth: 450, maxHeight: .infinity)
+                    .padding(.bottom)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.white)
+                            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                    )
                     
+                    VStack {
+                        Text("Work Base:")
+                            .font(.title)
+                            .padding()
+                        Text("\(eachHero.work.base)")
+                            .font(.title3)
+                            .lineLimit(nil)
+                            .multilineTextAlignment(.center)
+                        Text("First Appearance: \(eachHero.biography.firstAppearance)")
+                            .font(.title3)
+                            .lineLimit(nil)
+                            .multilineTextAlignment(.center)
+                    }
+                    .frame(maxWidth: 450, maxHeight: .infinity)
+                    .padding(.bottom)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.white)
+                            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                    )
                     
+                    VStack {
+                        Text("First Appearance:")
+                            .font(.title)
+                            .padding()
+                        Text("\(eachHero.biography.firstAppearance)")
+                            .font(.title3)
+                            .lineLimit(nil)
+                            .multilineTextAlignment(.center)
+                    }
+                    .frame(maxWidth: 450, maxHeight: .infinity)
+                    .padding(.bottom)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.white)
+                            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                    )
+                    
+                    VStack {
+                        Text("Group Affiliation:")
+                            .font(.title)
+                            .padding()
+ 
+                        Text("\(eachHero.connections.groupAffiliation)")
+                            .font(.title3)
+                            .lineLimit(nil)
+                            .multilineTextAlignment(.center)
+
+                    }
+                    .frame(maxWidth: 450, maxHeight: .infinity)
+                    .padding(.bottom)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.white)
+                            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                    )
+                    
+                    VStack {
+                        Text("Relatives:")
+                            .font(.title)
+                            .padding()
+                        
+                        Text("\(eachHero.connections.relatives)")
+                            .font(.title3)
+                            .lineLimit(nil)
+                            .multilineTextAlignment(.center)
+                    }
+                    .frame(maxWidth: 450, maxHeight: .infinity)
+                    .padding(.bottom)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.white)
+                            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                    )
+                    
+                    VStack {
+                        Text("Other:")
+                            .font(.title)
+                            .padding()
+                        Text("Publisher: \(eachHero.biography.publisher)")
+                            .font(.title3)
+                            .lineLimit(nil)
+                            .multilineTextAlignment(.center)
+                        Text("Alignment: \(eachHero.biography.alignment)")
+                            .font(.title3)
+                            .lineLimit(nil)
+                            .multilineTextAlignment(.center)
+                    }
+                    .frame(maxWidth: 450, maxHeight: .infinity)
+                    .padding(.bottom)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.white)
+                            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                    )
+    
                 }
-                .padding(.horizontal)
-                Group {
-                    Text("Hair Color: \(eachHero.appearance.hairColor)")
-                        .font(.title3)
-                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    Text("Occupation: \(eachHero.work.occupation)")
-                        .font(.title3)
-                        .multilineTextAlignment(.center)
-                    Text("Base: \(eachHero.work.base)")
-                        .font(.title3)
-                        .multilineTextAlignment(.center)
-                    Text("First Appearance: \(eachHero.biography.firstAppearance)")
-                        .font(.title3)
-                        .multilineTextAlignment(.center)
-                    Text("Publisher: \(eachHero.biography.publisher)")
-                        .font(.title3)
-                        .multilineTextAlignment(.center)
-                    Text("Alignment: \(eachHero.biography.alignment)")
-                        .font(.title3)
-                        .multilineTextAlignment(.center)
-                    Text("Group Affiliation: \(eachHero.connections.groupAffiliation)")
-                        .font(.title3)
-                        .multilineTextAlignment(.center)
-                    Text("Relatives: \(eachHero.connections.relatives)")
-                        .font(.title3)
-                        .multilineTextAlignment(.center)
-                    
-                    
-                }
-                .padding(.horizontal)
+                .padding()
             }
-
         }
-
     }
 }
 
 var dumbHero = [heroInfo(id: "1", name: "Kam", image: heroImage(url: "background"), biography: heroBiography(fullName: "Kamyar Dabiri", alterEgos: "None", placeOfBirth: "USA", firstAppearance: "dc", publisher: "ME", alignment: "GOOD" ), appearance: Appearance(gender: "Male", race: "White", height: ["6'10"], weight: ["200"], eyeColor: "balck", hairColor: "Blonde"), work: heroWork(occupation: "-", base: "CA"), connections: Connection(groupAffiliation: "Group", relatives: "relatives"), powerstats: Powerstats(intelligence: "1", strength: "1", speed: "1", durability: "1", power: "1", combat: "1"))]
 
-//struct HeroDetailsView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        HeroDetailsView(eachHero: dumbHero.first!)
-//    }
-//}
+struct HeroDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeroDetailsView(eachHero: dumbHero.first!)
+    }
+}
