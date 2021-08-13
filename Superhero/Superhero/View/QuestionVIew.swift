f61aa2ba3cac04fd9ccbc6bf74fa0429f1f87689
Kamyar Dabiri <kdabiria@uci.edu>
2021-08-13 15:35:46 -0700

    power question result page

--- Superhero/Superhero/View/QuestionVIew.swift
+++ Superhero/Superhero/View/QuestionVIew.swift
@@ -16,10 +16,89 @@ struct QuestionVIew: View {
     @State private var submitDisable = false
     @State var showHero = false
     
+    @Environment(\.presentationMode) var present
+    
+    @State var heroData: heroInfo = heroInfo(id: "1", name: "", image: heroImage(url: "background"), biography: heroBiography(fullName: "", alterEgos: "", placeOfBirth: "", firstAppearance: "", publisher: "", alignment: "" ), appearance: Appearance(gender: "", race: "", height: [""], weight: [""], eyeColor: "", hairColor: ""), work: heroWork(occupation: "", base: ""), connections: Connection(groupAffiliation: "", relatives: ""), powerstats: Powerstats(intelligence: "", strength: "", speed: "", durability: "", power: "", combat: ""))
+    
+    func getData(nameOfHero: String) {
+//        let urlString = "https://superheroapi.com/api/5750045291732827/search/\(nameOfHero)"
+        let urlString = "https://superheroapi.com/api/5750045291732827/\(nameOfHero)"
+        let url = URL(string: urlString)
+        
+        URLSession.shared.dataTask(with: url!) { data, _, error in
+            DispatchQueue.main.async {
+                if data != nil {
+                    do {
+                        let decoder = JSONDecoder()
+                        let decodedData = try decoder.decode(heroInfo.self, from: data!)
+                        self.heroData = decodedData
+                    } catch {
+                        print("Error! \(error)")
+                    }
+                }
+            }
+        }.resume()
+    }
+    
+    func powerSup(){
+        var randomHeroArray: [String] = []
+        var highestPower: [String] = []
+        
+        for _ in 1...3 {
+            var randomHero: String = allHero.randomElement()!
+//            randomHeroArray[0] = randomHero!
+            getData(nameOfHero: allHeroDict[randomHero]!)
+            highestPower.append(heroData.powerstats.power)
+            if heroData.powerstats.power == "null" {
+                continue
+            }
+            else if randomHeroArray.isEmpty {
+                print("here")
+                highestPower.append(heroData.powerstats.power)
+                randomHeroArray.append(randomHero)
+            }
+            else {
+                print("arry is \(randomHeroArray.isEmpty)")
+                if Int(randomHeroArray[0]) ?? 0 < Int(heroData.powerstats.power) ?? 0{
+                    highestPower[0] = heroData.powerstats.power
+                    randomHeroArray[0] = randomHero
+                }
+            }
+        }
+        
+//        print("array of random hero: \(randomHeroArray)")
+        
+//        return heroData
+    
+    }
+    
     var body: some View {
         if showHero {
-            VStack {
-                Text("YOUR ARE Superman")
+            VStack(spacing: 20) {
+                Text("You are \(heroData.name)")
+                Spacer()
+                Image(systemName: "eye.slash")
+                    .data(url: URL(string: heroData.image.url)!)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 300)
+                    .cornerRadius(12)
+                
+                Spacer()
+                
+                Button(action: {
+                    present.wrappedValue.dismiss()
+                }, label: {
+                    Text("Go Quiz")
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color("text"))
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 150)
+                        .background(Color.blue)
+                        .cornerRadius(25)
+
+                })
+                
             }
         }
         else {
@@ -79,6 +158,7 @@ struct QuestionVIew: View {
                             if index == 3{
                                 toDisable = true
                                 showHero = true
+                                powerSup()
                             }
                             else {
                                 toDisable = false
