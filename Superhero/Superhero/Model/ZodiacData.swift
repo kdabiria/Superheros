dce459b2c44dbb21624c46a192b79ee791835d3f
Kamyar Dabiri <kdabiria@uci.edu>
2021-08-18 08:31:58 -0700

    add zodic ui and data

---
+++ Superhero/Superhero/Model/ZodiacData.swift
@@ -0,0 +1,13 @@
+//
+//  ZodiacContainer.swift
+//  Superhero
+//
+//  Created by Kamyar Dabiri on 8/17/21.
+//
+
+import Foundation
+
+
+struct <#name#> {
+    <#fields#>
+}
---
+++ Superhero/Superhero/View/Zodiac.swift
@@ -0,0 +1,20 @@
+//
+//  Zodiac.swift
+//  Superhero
+//
+//  Created by Kamyar Dabiri on 8/17/21.
+//
+
+import SwiftUI
+
+struct Zodiac: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Zodiac_Previews: PreviewProvider {
+    static var previews: some View {
+        Zodiac()
+    }
+}
