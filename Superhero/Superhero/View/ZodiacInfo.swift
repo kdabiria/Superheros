adf839540ea878ec4c235d23203a215fa593b11f
Kamyar Dabiri <kdabiria@uci.edu>
2021-08-18 14:01:52 -0700

    added zodiac image and info

---
+++ Superhero/Superhero/View/ZodiacInfo.swift
@@ -0,0 +1,20 @@
+//
+//  ZodiacInfo.swift
+//  Superhero
+//
+//  Created by Kamyar Dabiri on 8/18/21.
+//
+
+import SwiftUI
+
+struct ZodiacInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ZodiacInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        ZodiacInfo()
+    }
+}
