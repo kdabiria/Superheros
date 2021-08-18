64fa7632b32e8cb07c3d2c9ce0de3896e0d75cdf
Kamyar Dabiri <kdabiria@uci.edu>
2021-08-18 08:32:47 -0700

    add zodic ui and data

--- Superhero/Superhero/Model/ZodiacData.swift
+++ Superhero/Superhero/Model/ZodiacData.swift
@@ -8,6 +8,31 @@
 import Foundation
 
 
-struct <#name#> {
-    <#fields#>
+struct zodiacNames: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
 }
+
+var zodicImg = [
+    zodiacNames(name: "Virgo", image: "virgo"),
+    zodiacNames(name: "Taurus", image: "taurus"),
+    zodiacNames(name: "Scorpio", image: "scorpio")
+]
+var zodicImg2 = [
+    zodiacNames(name: "Sagittarius", image: "sagittarius"),
+    zodiacNames(name: "Pisces", image: "pisces"),
+    zodiacNames(name: "Libra", image: "libra")
+]
+
+var zodicImg3 = [
+    zodiacNames(name: "Leo", image: "leo"),
+    zodiacNames(name: "Gemini", image: "gemini"),
+    zodiacNames(name: "Capricorn", image: "capricorn")
+]
+var zodicImg4 = [
+    zodiacNames(name: "Cancer", image: "cancer"),
+    zodiacNames(name: "Aries", image: "aries"),
+    zodiacNames(name: "Aqvarius", image: "aqvarius")
+]
+
--- Superhero/Superhero/View/Zodiac.swift
+++ Superhero/Superhero/View/Zodiac.swift
@@ -8,13 +8,91 @@
 import SwiftUI
 
 struct Zodiac: View {
+    @State var tap = false
+    
+    var counter = 1
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack() {
+            
+            LinearGradient(gradient: Gradient(colors: [Color("Start"), Color("Middle"), Color("End")]), startPoint: .top, endPoint: .bottom)
+            
+            VStack() {
+                Spacer()
+                Text("Zodiac Signs")
+                    .font(.system(size: 56, weight: .bold, design: .serif))
+                    .italic()
+                    .foregroundColor(.white)
+                    .padding(.vertical,50.0)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                VStack(spacing: 20) {
+                    
+                    HStack(spacing: 30) {
+                        ForEach(zodicImg) { img in
+                            CircleButton(item: img, tap: $tap)
+                        }
+                    }
+                    
+                    HStack(spacing: 30) {
+                        ForEach(zodicImg2) { img in
+                            CircleButton(item: img, tap: $tap)
+                        }
+                    }
+                    
+                    HStack(spacing: 30) {
+                        ForEach(zodicImg3) { img in
+                            CircleButton(item: img, tap: $tap)
+                        }
+                    }
+                    
+                    HStack(spacing: 30) {
+                        ForEach(zodicImg4) { img in
+                            CircleButton(item: img, tap: $tap)
+                        }
+                    }
+                }
+                .padding()
+                
+                Spacer()
+                Spacer()
+            }
+        }
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct Zodiac_Previews: PreviewProvider {
     static var previews: some View {
         Zodiac()
+    }
+}
+
+struct CircleButton: View {
+    var item: zodiacNames
+    @Binding var tap: Bool
+    var body: some View {
+        
+        Button(action: {
+            tap.toggle()
+        }, label: {
+            VStack {
+                Image(item.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .background(Color.black.opacity(0.5))
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                Text(item.name)
+                    .font(.system(.headline, design: .serif))
+                    .foregroundColor(.white)
+            }
+            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
+        })
+        .sheet(isPresented: $tap, content: {
+            Text("nothing yet")
+        })
     }
 }
