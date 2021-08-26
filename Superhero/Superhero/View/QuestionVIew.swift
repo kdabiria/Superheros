36282ded09055746af2b167a294bf0fe3c93ff60
Kamyar Dabiri <kdabiria@uci.edu>
2021-08-25 17:26:01 -0700

    added view for other quiz sections

--- Superhero/Superhero/View/QuestionVIew.swift
+++ Superhero/Superhero/View/QuestionVIew.swift
@@ -128,10 +128,86 @@ struct QuestionVIew: View {
                     }
                     .padding()
                 }
-                else {
-                    VStack {
-                        Text("no logic yet")
+                else if lvl == "2" {
+                    VStack(spacing: 20) {
+                        Text("You are \(heroData.name)")
+                            .font(.title2)
+                        if heroData.powerstats.speed != "null" {
+                            Text("Speed: \(heroData.powerstats.speed)")
+                                .font(.title2)
+                        }
+                        else {
+                            Text("Speed: -")
+                                .font(.title2)
+                        }
+                        
+                        Spacer()
+                        
+                        Image(systemName: "eye.slash")
+                            .data(url: URL(string: heroData.image.url)!)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 300)
+                            .cornerRadius(12)
+                        
+                        Spacer()
+                        
                     }
+                    .padding()
+                }
+                else if lvl == "3" {
+                    VStack(spacing: 20) {
+                        Text("You are \(heroData.name)")
+                            .font(.title2)
+                        if heroData.powerstats.intelligence != "null" {
+                            Text("Intelligence: \(heroData.powerstats.intelligence)")
+                                .font(.title2)
+                        }
+                        else {
+                            Text("Intelligence: -")
+                                .font(.title2)
+                        }
+                        
+                        Spacer()
+                        
+                        Image(systemName: "eye.slash")
+                            .data(url: URL(string: heroData.image.url)!)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 300)
+                            .cornerRadius(12)
+                        
+                        Spacer()
+                        
+                    }
+                    .padding()
+                }
+                else if lvl == "4" {
+                    VStack(spacing: 20) {
+                        Text("You are \(heroData.name)")
+                            .font(.title2)
+                        if heroData.powerstats.combat != "null" {
+                            Text("Combat: \(heroData.powerstats.combat)")
+                                .font(.title2)
+                        }
+                        else {
+                            Text("Combat: -")
+                                .font(.title2)
+                        }
+                        
+                        Spacer()
+                        
+                        Image(systemName: "eye.slash")
+                            .data(url: URL(string: heroData.image.url)!)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 300)
+                            .cornerRadius(12)
+                        
+                        Spacer()
+                        
+                    }
+                    .padding()
                 }
             }
         }
