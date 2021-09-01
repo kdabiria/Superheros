d63a40f469058e53ecad5be38274764bcfd21f0a
Kamyar Dabiri <kdabiria@uci.edu>
2021-09-01 09:01:21 -0700

    separate multi view

--- Superhero/Superhero/View/MyListView.swift
+++ Superhero/Superhero/View/MyListView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct MyListView: View {
+
+    @State var heroData = heroInfo(id: "1", name: "", image: heroImage(url: "background"), biography: heroBiography(fullName: "", alterEgos: "", placeOfBirth: "", firstAppearance: "", publisher: "", alignment: "" ), appearance: Appearance(gender: "", race: "", height: [""], weight: [""], eyeColor: "", hairColor: ""), work: heroWork(occupation: "", base: ""), connections: Connection(groupAffiliation: "", relatives: ""), powerstats: Powerstats(intelligence: "", strength: "", speed: "", durability: "", power: "", combat: ""))
+    
+    @State var isLoading = false
+    func getData(nameOfHero: String) {
+        isLoading = true
+        let urlString = "https://superheroapi.com/api/5750045291732827/\(nameOfHero)"
+        let url = URL(string: urlString)
+        
+        URLSession.shared.dataTask(with: url!) { data, _, error in
+            DispatchQueue.main.async {
+                if data != nil {
+                    do {
+                        let decoder = JSONDecoder()
+                        let decodedData = try decoder.decode(heroInfo.self, from: data!)
+                        self.heroData = decodedData
+                    } catch {
+                        print("Error! \(error)")
+                    }
+                }
+            }
+            isLoading = false
+        }.resume()
+    }
+    
+    var hero: String
+    @State var tapped = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(
+            destination: HeroDetailsView(eachHero: heroData, isLoading: $isLoading),
+            isActive: Binding<Bool> (get: {tapped}, set: {tapped = $0; getData(nameOfHero: allHeroDict[hero]!)}) ,
+            label: {
+                Text(hero)
+            })
     }
 }
 
-struct MyListView_Previews: PreviewProvider {
-    static var previews: some View {
-        MyListView()
-    }
-}
